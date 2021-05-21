72fbdcbb226a4367949245f128923868b2564dba
YUURIKATOU <swim.yuri255@gmail.com>
2021-05-21 13:13:06 +0900

    Project1の追加

---
+++ Project1/main.cpp
@@ -0,0 +1,15 @@
+#include <DxLib.h>
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	DxLib::SetGraphMode(1280, 720, 32);
+	DxLib::ChangeWindowMode(true);
+	DxLib::SetWindowTextW(L"1916049_‰Á“¡—D—˜");
+	if (DxLib_Init()){
+		return -1;
+	}
+	DxLib::SetDrawScreen(DX_SCREEN_BACK);
+	while (DxLib::ProcessMessage() == 0) {
+		DxLib::ScreenFlip();
+	}
+	return 0;
+}
